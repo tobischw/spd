1710d47b7405537c18aeddef1e550c9357978bd8
Will <willpat15@gmail.com>
2020-05-20 19:20:41 -0500

    Working on some simple fileio

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 mod document;
 mod asset;
+mod fileio;
 
 pub use asset::Asset;
 pub use document::Document;
+pub use fileio::FileIO;
--- tests/basic.rs
+++ tests/basic.rs
@@ -3,10 +3,21 @@ extern crate spd;
 #[test]
 fn create_doc() {
     let doc = spd::Document::new();
-
     assert_eq!(doc.head.min_version, 0x01)
 }
 
-fn create_doc_with_asset() {
-    
+#[test]
+fn create_fileio() {
+    let y = spd::FileIO::new(String::from("yeet.txt"));
+    assert_eq!(y.file, String::from("yeet.txt"))
 }
+
+#[test]
+fn read_file(){
+    let y = spd::FileIO::new(String::from("./res/index.html"));
+    let d = y.read_file().unwrap();
+    println!("{:?}", d)
+}
+
+
+
